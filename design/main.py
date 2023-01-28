c653ead3e3174cdea18e4c1f4bb142636759d820
Nigel Wright <nigeljwright22@gmail.com>
2023-01-28 11:46:11 -0600

    experimenting with kivy for design ui

--- design/main.py
+++ design/main.py
@@ -1,1 +1,30 @@
-# UI app code goes here!
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+
+
+class MyLayout(Widget):
+
+    def __init__(self, **kwargs):
+        super(MyLayout, self).__init__(**kwargs)
+        self.tree_btn = self.ids.tree_btn
+
+    def add_tree(self, touch):
+        self.img = Image(source='images/tree1.png', pos=touch.pos)
+        self.add_widget(self.img)
+        self.img.pos = touch.pos
+
+    def on_touch_move(self, touch):
+        try:
+            self.img.pos = touch.pos
+        except AttributeError:   # if image hasn't been created yet (button not pressed)
+            pass
+
+
+class MainApp(App):
+    def build(self):
+        return MyLayout()
+
+
+if __name__ == '__main__':
+    MainApp().run()
