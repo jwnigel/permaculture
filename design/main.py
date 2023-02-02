238f6631e71d37a64f5851466fe42da39ea4a84e
Nigel Wright <nigeljwright22@gmail.com>
2023-02-02 01:04:09 -0600

    fleshing out template

--- design/main.py
+++ design/main.py
@@ -1,7 +1,10 @@
 from kivy.config import Config
-# Config.set('graphics', 'window_state', 'maximized') # Maximize window on startup
+Config.set('graphics', 'window_state', 'maximized') # Maximize window on startup
 
 from kivymd.app import MDApp
+from kivymd.uix.relativelayout import MDRelativeLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.behaviors import ButtonBehavior
@@ -9,56 +12,54 @@ from kivy.uix.behaviors import DragBehavior
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.graphics import Rectangle
 
-from kivy.uix.stencilview import StencilView
+BACKGROUND_IMAGE = 'images/largefield1.png'
 
+class MainScreen(Screen):
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        self.layout = MasterLayout()
+        self.add_widget(self.layout)
 
-class DraggableImage(DragBehavior, Image):
+class MasterLayout(MDBoxLayout):
     pass
 
-class IconButton(ButtonBehavior, Image):
-
-    def __init__(self, **kwargs):
-        super(IconButton, self).__init__(**kwargs)
-
-class MasterLayout(BoxLayout):
+class LeftPanel(BoxLayout):
     pass
 
-class BackgroundImage(Image):
-    def adjust_image_size(self, stencil):
-        stencil_ratio = stencil.width / float(stencil.height)
-        if self.image_ratio > stencil_ratio:
-            self.width = stencil.height * self.image_ratio
-            self.height = stencil.height
-        else:
-            self.width = stencil.width
-            self.height = stencil.width / self.image_ratio
-
-class MyStencilView(RelativeLayout, StencilView):
+class DesignPanel(MDRelativeLayout):
 
     def __init__(self, **kwargs):
-        super(MyStencilView, self).__init__(**kwargs)
+        super(DesignPanel, self).__init__(**kwargs)
+        self.background = BACKGROUND_IMAGE
 
-    def on_size(self, *args):
-        self.children[0].adjust_image_size(self)
-
-    def add_tree(self, size, pos):
-        tree = DraggableImage(source='images/tree1.png',
+    def add_plant(self, size, pos):
+        plant = DraggableImage(source='images/tree1.png',
                               size_hint=(None, None),
                               size=size,
                               pos=pos)
         # tree.on_motion(me=True, etype='begin') #This didn't work to make drag default
-        self.add_widget(tree)
+        self.add_widget(plant)
 
     def activate_drag(self, instance):
         # to-do
         pass
 
+class DraggableImage(DragBehavior, Image):
+    pass
+
+class IconButton(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super(IconButton, self).__init__(**kwargs)
+
+
+
 class MainApp(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Green"
-        return MasterLayout()
-
+        root=ScreenManager()
+        root.add_widget(MainScreen(name='main'))
+        return root
 
 if __name__ == '__main__':
     MainApp().run()
---
+++ design/nav_drawer.py
@@ -0,0 +1,86 @@
+from kivy.lang import Builder
+
+from kivymd.app import MDApp
+
+KV = '''
+<DrawerClickableItem@MDNavigationDrawerItem>
+    focus_color: "#e7e4c0"
+    text_color: "#4a4939"
+    icon_color: "#4a4939"
+    ripple_color: "#c5bdd2"
+    selected_color: "#0c6c4d"
+
+
+<DrawerLabelItem@MDNavigationDrawerItem>
+    text_color: "#4a4939"
+    icon_color: "#4a4939"
+    focus_behavior: False
+    selected_color: "#4a4939"
+    _no_ripple_effect: True
+
+
+MDScreen:
+
+    MDNavigationLayout:
+
+        MDScreenManager:
+
+            MDScreen:
+
+                MDTopAppBar:
+                    title: "Navigation Drawer"
+                    elevation: 4
+                    pos_hint: {"top": 1}
+                    md_bg_color: "#e7e4c0"
+                    specific_text_color: "#4a4939"
+                    right_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+
+        MDNavigationDrawer:
+            id: nav_drawer
+            radius: (0, 16, 16, 0)
+            anchor: 'right'
+
+            MDNavigationDrawerMenu:
+
+                MDNavigationDrawerHeader:
+                    title: "Header title"
+                    title_color: "#4a4939"
+                    text: "Header text"
+                    spacing: "4dp"
+                    padding: "12dp", 0, 0, "56dp"
+
+                MDNavigationDrawerLabel:
+                    text: "Mail"
+
+                DrawerClickableItem:
+                    icon: "gmail"
+                    right_text: "+99"
+                    text_right_color: "#4a4939"
+                    text: "Inbox"
+
+                DrawerClickableItem:
+                    icon: "send"
+                    text: "Outbox"
+
+                MDNavigationDrawerDivider:
+
+                MDNavigationDrawerLabel:
+                    text: "Labels"
+
+                DrawerLabelItem:
+                    icon: "information-outline"
+                    text: "Label"
+
+                DrawerLabelItem:
+                    icon: "information-outline"
+                    text: "Label"
+'''
+
+
+class Example(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_string(KV)
+
+
+Example().run()
