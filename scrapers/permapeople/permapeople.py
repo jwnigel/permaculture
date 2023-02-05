0cb7b71cd8bef1a730588e1e7ade30b6bbc62423
berteh <36950+berteh@users.noreply.github.com>
2023-02-05 04:16:10 +0100

    added command line arguments

--- scrapers/permapeople/permapeople.py
+++ scrapers/permapeople/permapeople.py
@@ -4,11 +4,11 @@
 # licensed under terms of CC-BY
 
 import requests, pandas as pd, json, re
-
+import argparse
 
 API_KEY_ID="Dbt02E5ZLbY4"
 API_KEY_SECRET="f3b7ede2-2083-4163-b1c1-ca77e2814c5a"
-DEBUG = True
+DEBUG = False
 
 CONTACT="Ben <hello@permapeople.org>"
 HEADERS={'x-permapeople-key-id':API_KEY_ID,
@@ -28,8 +28,40 @@ DATA_COLUMNS_ORDER = ['id', 'name', 'slug', 'data', 'description', 'created_at',
        'data.Habitat', 'data.Warning', 'data.Native to', 'data.Introduced into', 
        'data.Wikipedia', 'data.Plants For A Future', 'data.Plants of the World Online Link',
        'data.1000 Seed Weight (g)']
-DATA_COLUMNS_KEEP_OTHERS = True
 DATA_PREFIX="data."   #set "" to loose the permapeople JSON second level information and make labels directly readable
+
+
+
+# parse options
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description=''' Scrape plants data from permapeople.org''',
+                                 usage="%(prog)s [options]",
+                                 epilog='''requirements
+    This program requires Python 3.0+
+
+examples:
+
+  %(prog)s -t "full sun ground cover" -v
+     generates "full sun ground cover.csv" (and .json) from the full text search results."
+
+  %(prog)s -v -f design-john.txt
+     generates "design-john.csv" from scraping data for all plants in "design-john.txt", while printing a verbose progress feedback.
+
+more information: https://github.com/jwnigel/permaculture
+ ''')
+
+parser.add_argument('-t', '--text', default=False,
+                    help='Text you want to get data for via full text search. Result is saved in a file with same name.')
+parser.add_argument('-f', '--file', default=False,
+                    help='TXT file of list of plants you want to get data for via full text search. Default is "plants.txt".')
+parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                    help='print more (text) info to command line')
+args = parser.parse_args()
+VERBOSE = args.verbose or DEBUG
+
+
+
+
 
 
 
@@ -43,7 +75,7 @@ def list(last: int = 0) -> list:
         list of the first next 100 plants, starting after the {last} id.
     """
 	
-	if DEBUG: print(f"* listing 100 plants from {last} on, please be patient")
+	if VERBOSE: print(f"* listing 100 plants from {last} on, please be patient")
 	r = requests.get("https://permapeople.org/api/plants", headers=AUTH, 
 		params={'last':last})
 	return ifNoHttpError(r)
@@ -62,18 +94,18 @@ def search(text: str = "Malus pumila", max_results: int = -1, file_store: str = 
         Array of the first next 100 plants, starting after the {last} id.
     """
 
-	if DEBUG: print(f"* searching for {text}.")
+	if VERBOSE: print(f"* searching for {text}.")
 	r = requests.post("https://permapeople.org/api/search", headers=HEADERS, 
 		params={'q':text})
 	plants = ifNoHttpError(r)['plants']
-	if DEBUG:
+	if VERBOSE:
 		print(f"  found {len(plants)} plants, returning {max_results}.")
 	
 	if (file_store != ""): # save to file, TODO make beautify from JSON optional for more performance
 		json_object = json.dumps(plants, indent=2)
 		with open(file_store, "w") as outfile:
 			outfile.write(json_object)
-		if DEBUG: print(f"  saved search result to {file_store}.")
+		if VERBOSE: print(f"  saved search result to {file_store}.")
 	
 	return plants if (max_results==-1) else plants[:max_results]
 
@@ -89,18 +121,17 @@ def update(id, content): #content must be an array of dict such as [{"key": "Edi
 
 
 def searchList(file: str = "plants.txt", separator: str = "|"):
-	if DEBUG: print(f"* gathering data for all plants in {file}.")
+	if VERBOSE: print(f"* gathering data for all plants in {file}.")
 	t = pd.read_csv(file, sep = separator, skipinitialspace = True, header = None, 
 		names=["latin"], converters={'latin':simplifySpaces}).to_numpy(dtype = str)
 	frames = []
 	for [p] in t	:  #TODO iterate is the worst performance on DF... got a way to vectorize the operation of search & combine ?
 		found = search(text = p, max_results = 1, file_store = f"{p}.json")[0]
 		df = permaPeopleJsonToDf([found]) # found -> [found] since method expects an array, and json_normalize cleans the array if single element.
-		df.drop(columns='data', inplace = True) # comment to keep original data attribute... but all its content has been copied to first level attributes anyway.
 		frames.append(df)
 	
 	if DEBUG: print(f"  merging {len(frames)} records")
-	details = pd.concat(frames, copy = False, ignore_index=True) # HERE the concat does not concat...
+	details = pd.concat(frames, copy = False, ignore_index=True)
 	return details
 
 
@@ -123,12 +154,13 @@ def permaPeopleJsonToDf(plants: list) -> list:
 			p[DATA_PREFIX+d['key']]=d['value']			
 	
 	df = pd.json_normalize(plants) # TODO Here I need to tell DF to use "id" as index, so that duplicates are discarded on concat. How ?
+	df.drop(columns='data', inplace = True) # comment to keep original data attribute... but all its content has been copied to first level attributes anyway.
 	if DEBUG: print(f"  normalized headers are {df.columns[:4]}.")
 	
 	return df
 
 def loadJson(file: str ="plants.json"):
-	if DEBUG: print(f"* loading data from {file}.")
+	if VERBOSE: print(f"* loading data from {file}.")
 	data = pd.read_json(file)
 	#if DEBUG: print(f"  loaded data of type {type(data)}.")
 	return data
@@ -152,18 +184,27 @@ def simplifySpaces(text: str):
 
 
 
-## run app ################################################
+## run the app ################################################
 
-#j = search("Morus alba", file_store="Morus alba.json")
-#t = permaPeopleJsonToDf(j)
-#print(f"single search result :\n{t}.")
+if(args.text) :
+	json = search(args.text, file_store= f"{args.text}.json")
+	df = permaPeopleJsonToDf(json)
+	df.to_csv(f"{args.text}.csv")
+	if VERBOSE: print(f"single search result :\n{df}.")
+	print(f"text search result is available in '{args.text}.csv'")
 
 
+# todo add --load option to merge multiple permapeople.json's files from command line, eg full search history.
 #t = loadJson("malus_pumila.json")
 #t = permaPeopleJsonToDf(j)
 #print(f"result from loading from local search history file :\n{t}.")
 
 
-t = searchList(file="../pfaf/test_plants.txt")
-if DEBUG: print(f"multiple plants search at once:\n{t}.")
+if(args.file) :
+	json = searchList(file=args.file)
+	df = permaPeopleJsonToDf(json)
+	outf = args.file.rsplit(".",1)[0]
+	df.to_csv(f"{outf}.csv")
+	if VERBOSE: print(f"list search result :\n{df}.")
+	print(f"list search result is available in '{outf}.csv'")
 
