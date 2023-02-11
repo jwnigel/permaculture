059e497debab4bb120f2388e82d89925ed3c7703
Nigel Wright <nigeljwright22@gmail.com>
2023-02-11 17:39:26 -0600

    Selection of plant displays its attributes. New search clears prior selection.

--- play/search.py
+++ play/search.py
@@ -1,6 +1,5 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.recycleview import RecycleView
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
@@ -10,13 +9,13 @@ from kivy.uix.behaviors import FocusBehavior
 from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from plantdata import PlantData
 
-
 plant_data = PlantData()
 
 class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
                                  RecycleBoxLayout):
     ''' Adds selection and focus behaviour to the view. '''
-
+    # allow deleselection ***How can I make this clear plant_attr text?
+    touch_deselect_last = BooleanProperty(True)
 
 class SelectableLabel(RecycleDataViewBehavior, Label):
     ''' Add selection support to the Label '''
@@ -43,30 +42,47 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
         self.selected = is_selected
         if is_selected:
             print("selection changed to {0}".format(rv.data[index]))
-            app.root.ids.plant_attrs.text = rv.data[index]['text']
-
+            rv.get_selected()
+            return index
 
 class RV(RecycleView):
     def __init__(self, **kwargs):
         super(RV, self).__init__(**kwargs)
-        self.results = []
-        self.data = [{'text': res} for res in range(len(self.results))]
+        self.data = []
+
+    def get_selected(self):
+        app = App.get_running_app()
+        if len(self.layout_manager.selected_nodes) > 0:
+            all_attrs = str(app.root.results[self.data[self.layout_manager.selected_nodes[-1]]['text']])
+            attrs_label = app.root.ids['plant_attrs']
+            attrs_label.text = all_attrs
+        else:
+            return ''
 
 class MainLayout(BoxLayout):
 
-    def search(self, value):
-        results = plant_data.call_plant(value.text)
-        if len(results) > 0:
-            self.ids.rv.data = [{'text': res} for res in results]
-        print(f'search results: {results}')
-        print(self.ids.rv.data)
+    def search(self, value): # called from TextInput on_text_validate (Enter)
+        self.results = {plant_series[1]['CommonName']: plant_series[1].drop(columns='CommonName')\
+            for plant_series in plant_data.call_plant(value.text).iterrows()}
+
+        rv = self.ids.rv
+        if len(self.results) > 0:
+            rv.data = [{'text': plant} for plant in self.results.keys()]
+        else:
+            rv.data = [{'text': 'No results found'}]
+
+        # Unselect RecycleView selection if selected
+        try:
+            rv.layout_manager.deselect_node(rv.layout_manager.selected_nodes[-1])
+        except IndexError:
+            pass
+
+        # Clear plant attributes from previous search
+        self.ids.plant_attrs.text = ''
 
 class SearchApp(App):
     def build(self):
         return MainLayout()
-
-
-
 
 if __name__ == '__main__':
     SearchApp().run()
